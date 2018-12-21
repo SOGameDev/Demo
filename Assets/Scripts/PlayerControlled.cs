7e0fa110500a0f1510375ee906320f63805b0df4
Michael <michael.grand.2@gmail.com>
2018-12-21 13:53:03 -0800

    The frog's movement is now handled by the physics engine.

--- Assets/Scripts/PlayerControlled.cs
+++ Assets/Scripts/PlayerControlled.cs
@@ -1,6 +1,7 @@
 ﻿namespace SOGameDev.Demo
 {
     using UnityEngine;
+    using UnityEngine.Assertions;
 
     /// <summary>
     ///     Allows the player to control this game object with the horizontal and vertical axis.
@@ -11,12 +12,20 @@
         ///     The speed that the character should move at.
         /// </summary>
         public float speed = 0.15f;
-        
+
+        private Rigidbody2D rb;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            Assert.IsNotNull(rb, "PlayerControlled requires a Rigidbody2D component, but none was found.");
+        }
+
         private void FixedUpdate()
         {
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
-            transform.position = transform.position + speed*(new Vector3(horizontal, vertical, 0));
+            rb.AddForce(speed*(new Vector2(horizontal, vertical)), ForceMode2D.Impulse);
         }
     }
 }
