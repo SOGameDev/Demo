46a3e7e00e244ec5069a84e1d8fa8fdd31ff9b7d
Michael <michael.grand.2@gmail.com>
2018-12-21 13:42:49 -0800

    Licenses window can now be opened/closed.

---
+++ Assets/Scripts/HideShow.cs
@@ -0,0 +1,18 @@
+﻿namespace SOGameDev.Demo
+{
+    using UnityEngine;
+
+    /// <summary>
+    ///     Toggles this object's active state. This will hide an object if it is visible, and show the object if it is hidden.
+    /// </summary>
+    public class HideShow : MonoBehaviour
+    {
+        /// <summary>
+        ///     Call this to toggle the active (hidden/shown) state of this object.
+        /// </summary>
+        public void Toggle()
+        {
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
+    }
+}
