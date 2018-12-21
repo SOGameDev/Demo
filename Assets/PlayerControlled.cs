6c760ca9305e274d2fcb9a353911d62bbe489fd7
Michael <michael.grand.2@gmail.com>
2018-12-21 12:38:55 -0800

    Removed unused code.

--- Assets/PlayerControlled.cs
+++ Assets/PlayerControlled.cs
@@ -1,23 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerControlled : MonoBehaviour
 {
     public float speed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        //Debug.Log(direction);
         transform.position = transform.position + speed*(new Vector3(horizontal, vertical, 0));
     }
 }
