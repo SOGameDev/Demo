50aefa1f30ad4597f3c260f2a17cf768e8f49b3c
Michael Grand <michael.grand.2@gmail.com>
2019-01-24 20:30:01 -0800

    The game resets if the bear falls too far.

---
+++ Assets/Scripts/ResetGameOnFall.cs
@@ -0,0 +1,32 @@
+﻿namespace SOGameDev.Demo
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    /// <summary>
+    ///     Resets the game if the object falls too far below the ground.
+    /// </summary>
+    public class ResetGameOnFall : MonoBehaviour
+    {
+        /// <summary>
+        ///     The minimum distance below the ground that this object's center
+        ///     must be for the game to reset.
+        /// </summary>
+        public float distance = 2.0f;
+
+        private Transform ground;
+
+        private void Start()
+        {
+            ground = GameObject.FindWithTag("Ground").transform;
+        }
+
+        private void Update()
+        {
+            if (transform.position.y < ground.position.y - distance)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
