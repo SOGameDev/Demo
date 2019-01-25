727790630927af753ef9d15d6c2c4d3e1014aa0c
Brendan Wright <edgeloops@gmail.com>
2019-01-24 23:17:10 -0800

    some stuff..
    I ran into some odd problem with the bear head trail that will probably solve itself with a night's sleep.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    GameObject frog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        frog = GameObject.Find("Frog");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(
+
+                frog.transform.position.x,
+                frog.transform.position.y,
+                frog.transform.position.z - 5.0f
+
+        ), Time.deltaTime * 5.0f);
+
+    }
+}
--- Assets/Scripts/PlayerControlled.cs
+++ Assets/Scripts/PlayerControlled.cs
@@ -6,7 +6,7 @@
     /// <summary>
     ///     Allows the player to control this game object with the horizontal and vertical axis.
     /// </summary>
-    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Rigidbody))]
     public class PlayerControlled : MonoBehaviour
     {
         /// <summary>
@@ -14,24 +14,37 @@
         /// </summary>
         public float speed = 0.15f;
 
-        private Rigidbody2D rb;
+        private Rigidbody rb;
 
         private Vector2 heading;
+        GameObject bearTrail;
+
+        GameObject bear;
 
         private void Start()
         {
-            rb = GetComponent<Rigidbody2D>();
+            rb = GetComponent<Rigidbody>();
+            bear = GameObject.Find("BearTrail");
+            bearTrail = GameObject.Find("BearTrail");
             Assert.IsNotNull(rb, "PlayerControlled requires a Rigidbody2D component, but none was found.");
         }
 
         private void FixedUpdate()
         {
-            rb.AddForce(speed*heading, ForceMode2D.Impulse);
+            rb.AddForce(speed*heading, ForceMode.Impulse);
+        }
+
+        void OnCollisionEnter(Collision coll){
+            if(coll.gameObject.name == "Bear"){
+                Rigidbody rb = coll.gameObject.GetComponent<Rigidbody>();
+                if (rb != null){ rb.AddExplosionForce(1000.0f, coll.gameObject.transform.position, 2.0f, 3.0F); }
+            }
         }
 
         private void Update()
         {
             heading = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+            bearTrail.transform.position = bear.transform.position + new Vector3(0,0,-5);
         }
     }
 }
